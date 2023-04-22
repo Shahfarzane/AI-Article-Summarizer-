e31c6cf667fa1b13bab4b2d69662598765f83264
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2023-04-22 17:35:07 +0200

    Final version

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <div className="gradient" />
       </div>
 
-      <div className="app">
+      <div className="relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6">
         <Hero />
         <Demo />
       </div>
